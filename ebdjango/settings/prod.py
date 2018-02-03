101b0664460abd02d755133be46499241fbec928
Ruban Maher <maher@live.com.sg>
2018-02-04 00:34:04 +0800

    static location bug fixes

--- ebdjango/settings/prod.py
+++ ebdjango/settings/prod.py
@@ -59,12 +59,13 @@ AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
 
+AWS_LOCATION = 'static'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATICFILES_LOCATION = 'static'
+#STATICFILES_LOCATION = 'static'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
 

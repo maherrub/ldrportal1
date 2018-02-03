1f40a58604456f323220775ef978bd55954a742d
Ruban Maher <maher@live.com.sg>
2018-02-03 14:12:52 +0800

    Reg and Login Optimization
    Registratio and Login UI optimized

--- static/js/ldranimations.js
+++ static/js/ldranimations.js
@@ -8,6 +8,4 @@
 //TweenMax.to(".wrapper", 1, {width:"50%", repeat:3, repeatDelay:0.5});
 
 TweenLite.to(".wrapper", 2, {width:300, height:300, delay:2.6 });
-
-
-//TweenLite.to(bottomhome, 2, { ease: Elastic.easeOut.config(1, 0.3), y: -1 });
+TweenLite.to(".fixed-bottom", 3, {boxShadow:"0px 0px 20px red", delay:2.6, color:"#FC0"});

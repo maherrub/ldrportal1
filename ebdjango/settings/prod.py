3993bf609164a5f80e70bbea243d35f439d69d63
Ruban Maher <maher@live.com.sg>
2018-02-04 01:33:39 +0800

    static fix3

--- ebdjango/settings/prod.py
+++ ebdjango/settings/prod.py
@@ -59,21 +59,22 @@ AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
 
-#AWS_LOCATION = 'static'
-
+AWS_LOCATION = 'static'
 
 
 STATICFILES_LOCATION = 'static'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATIC_URL = 'https://%s/static/%s/' % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
+STATIC_URL = 'https://%s/%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION, STATICFILES_LOCATION)
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+AWS_LOCATION_MEDIA = 'media'
+
 MEDIAFILES_LOCATION = 'media'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-MEDIA_URL = "https://%s/media/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+MEDIA_URL = "https://%s/%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION_MEDIA, MEDIAFILES_LOCATION)
 
 
 #STATIC_URL = '/static/'

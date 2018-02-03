808c703924add87c2d3781f6507d8e8e492a209f
Ruban Maher <maher@live.com.sg>
2018-02-03 21:19:56 +0800

    S3 Bug Fixes
    Storage Access bug fixes added storage backends

---
+++ ebdjango/settings/storage_backends.py
@@ -0,0 +1,10 @@
+from .pro import *
+from storages.backends.s3boto3 import S3Boto3Storage
+
+
+#class StaticStorage(S3Boto3Storage):
+#    location = STATICFILES_LOCATION
+
+class MediaStorage(S3Boto3Storage):
+    location = MEDIAFILES_LOCATION
+    file_overwrite = False
